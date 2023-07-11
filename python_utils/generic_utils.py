495bd044f4734cac19842eadc80782aa72cb9aaa
JAMWINE <shauryakhurana07@gmail.com>
2023-07-10 21:44:55 -0500

    FIX: Updating encoding in save_output_in_json() in generic_utils.py

--- python_utils/generic_utils.py
+++ python_utils/generic_utils.py
@@ -3,9 +3,9 @@ import json
 
 def save_output_in_json(output_file_path, data, data_description = ''):
     try:
-        with open(output_file_path, 'w') as json_file:
+        with open(output_file_path, 'w', encoding='utf8') as json_file:
             if data_description != '':
-                json.dump({data_description: data}, json_file)
+                json.dump({data_description: data}, json_file, ensure_ascii=False)
             else:
                 json.dump({'data': data}, json_file)
         print(f"JSON file '{output_file_path}' saved successfully!\n")
